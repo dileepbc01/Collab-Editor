a29aa253ceb41811c941a00522876a4f26bc9ea8
Dileep B C <dileepbc01@gmail.com>
2025-04-28 19:36:18 +0530

    setup collab server template

--- backend/collab-server/app/config.py
+++ backend/collab-server/app/config.py
@@ -1,14 +1,10 @@
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
-import os
 
-env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
-load_dotenv(env_path)
-
+load_dotenv()  
 
 class Settings(BaseSettings):
     database_url: str
-    port: int = 8000
     echo_sql: bool = True
     test: bool = False
     project_name: str = "My FastAPI project"
--- backend/collab-server/app/main.py
+++ backend/collab-server/app/main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
-
 import logging
 import sys
 from contextlib import asynccontextmanager
-from app.config import settings
 
-# from app.api.routers.users import router as users_router
-# from app.database import sessionmanager
+import uvicorn
+from fastapi import FastAPI
+
+from app.api.routers.users import router as users_router
+from app.config import settings
+from app.database import sessionmanager
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if settings.log_level == "DEBUG" else logging.INFO)
 
@@ -18,12 +19,12 @@ async def lifespan(app: FastAPI):
     To understand more, read https://fastapi.tiangolo.com/advanced/events/
     """
     yield
-    # if sessionmanager._engine is not None:
-    #     # Close the DB connection
-    #     await sessionmanager.close()
+    if sessionmanager._engine is not None:
+        # Close the DB connection
+        await sessionmanager.close()
 
 
-app = FastAPI(lifespan=lifespan,  docs_url="/api/docs")
+app = FastAPI(lifespan=lifespan, title=settings.project_name, docs_url="/api/docs")
 
 
 @app.get("/")
@@ -32,4 +33,8 @@ async def root():
 
 
 # Routers
-# app.include_router(users_router)
+app.include_router(users_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
